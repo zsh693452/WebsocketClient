6b2fe785ef03e8e7177d63497af3cd52bc30c315
zsh693452 <zsh693452@163.com>
2024-02-05 18:00:52 +0800

    更新

--- test/windows/test/test.cpp
+++ test/windows/test/test.cpp
@@ -25,7 +25,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	int sendrst = WS_Send(handle, sendstr, strlen(sendstr), WS_DATA_TEXT, 1000 * 5);
 	printf("send rst=%d send %d bytes\n", sendrst, strlen(sendstr));
 
-	/*
+
 	char maxsendstr[1024] = {0};
 	memset(maxsendstr, 'A', sizeof(maxsendstr) - 1);
 	maxsendstr[1020] = 'D';
@@ -33,7 +33,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	maxsendstr[1022] = 'B';
 	sendrst = WS_Send(handle, maxsendstr, strlen(maxsendstr), 1000 * 5);
 	printf("send max rst=%d send %d bytes\n", sendrst, strlen(maxsendstr));
-	*/
+	
 
 	Sleep(1000 * 600);
 	WS_Close(handle);
